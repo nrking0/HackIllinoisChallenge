e711593e3032acda01a12c7b328203495e4488c7
Nick King <nrking0@gmail.com>
2020-09-16 17:11:55 -0500

    Imported Calendar data to UI

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import { ScheduleComponent, Day, Week, Month, Agenda, Inject, ViewsDirective, ViewDirective} from '@syncfusion/ej2-react-schedule';
+import React, { useState, useEffect } from 'react';
+import { ScheduleComponent, Day, Week, Month, Agenda, Inject, ViewsDirective, ViewDirective } from '@syncfusion/ej2-react-schedule';
 import '../../App.css'
 import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
 
@@ -8,47 +8,66 @@ const Calendar = (props) => {
 
     var read = true;
 
+    var newWholeJson = [];
 
-    var remoteData = new DataManager({
-        url: 'https://api.hackillinois.org/event/',
-        adaptor: new WebApiAdaptor(),
-        crossDomain: true
-    });
 
     const getEvents = async () => {
         try {
-          const response = await fetch("https://js.syncfusion.com/demos/ejservices/api/Schedule/loadData");
-          const jsonData = await response.json();
-          console.log(jsonData);
-          setEvents(jsonData);
-        } catch (err) {
-          console.error(err.message);
-        }
-      }
-    
-      useEffect(() => {
-        getEvents();
-      }, []);
+            const response = await fetch("https://api.hackillinois.org/event/");
+            const jsonData = await response.json();
+            console.log(jsonData);
+            for (var i = 0; i < jsonData.events.length; i++) {
+                var obj = jsonData.events[i];
+                var time = obj.startTime;
+                var tStart = new Date(time * 1000);
+                var tStartFormatted = tStart.toISOString();
+                tStartFormatted = tStartFormatted.substring(0, 19);
+                jsonData.events[i].startTime = tStartFormatted;
+                var timeEnd = jsonData.events[i].endTime;
+                var tEnd = new Date(timeEnd * 1000);
+                var tEndFormatted = tEnd.toISOString();
+                tEndFormatted = tEndFormatted.substring(0, 19);
+                jsonData.events[i].endTime = tEndFormatted;
+            }
+            // var temp = events;
+            // temp.push(jsonData);
+            // setEvents(temp);
+            var json = jsonData.events;
+            for(var i = 0; i < json.length; i ++) {
+                var newJson = {
+                    "Id": json[i].id,
+                    "Subject": json[i].name,
+                    "Description": json[i].description,
+                    "StartTime": json[i].startTime,
+                    "EndTime": json[i].endTime
+                }
+                newWholeJson.push(newJson);
+            }
 
-    return(
+            console.log(newWholeJson);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    useEffect(() => {
+        getEvents();
+    }, []);
+
+    return (
         <div>
-            <ScheduleComponent height='550px' selectedDate={new Date(2017, 5, 5)} readonly = {{read}}
-            eventSettings={{ dataSource: events,
-            /*fields: {
-                id: 'Id',
-                subject: { name: 'Subject' },
-                isAllDay: { name: 'IsAllDay' },
-                startTime: { name: 'StartTime' },
-                endTime: { name: 'EndTime' }
-            }*/
-            }}
+            <ScheduleComponent height='550px' selectedDate={new Date(2020, 7, 8)} readonly={{ read }}
+                eventSettings={{
+                    dataSource: newWholeJson,
+                }}
             >
-          <Inject services={[Day, Week, Month, Agenda]}/>
-          <ViewsDirective>
-                <ViewDirective option='Day'/>
-                <ViewDirective option='Week'/>
-            </ViewsDirective>
-        </ScheduleComponent>
+
+                <Inject services={[Day, Week, Month, Agenda]} />
+                <ViewsDirective>
+                    <ViewDirective option='Day' />
+                    <ViewDirective option='Week' />
+                </ViewsDirective>
+            </ScheduleComponent>
         </div>
     )
 };
