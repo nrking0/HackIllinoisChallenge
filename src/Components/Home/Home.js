cfb80bb79dfd47b955d206bae2564abc14338537
Nick King <nrking0@gmail.com>
2020-09-17 11:40:32 -0500

    Finshed Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Home.css';
+import Button from '@material-ui/core/Button';
 
 
 const Home = (props) => {
   return (
     <>
       <div className="root">
-        <h1>HackIllinois</h1>
+        <h1>HackThis</h1>
         <div className="below">
           <div className="about">
             <div className="aboutH">About</div>
-              <p>Hello!</p>
+              <div className="aboutP">This is my React Project for the HackIllinois Systems Front-End Challenge. I have utilized the HackIllinois API to have a schedule prepared with all the listed events. You can reach the schedule using the NavBar, or just with the link below. Enjoy!</div>
+              <Button className="button" variant="contained" color="primary" href="/schedule">View Schedule</Button>
           </div>
         </div>
       </div>
