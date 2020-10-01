c12d12fcd0d22937a84656b8f560a2908edad980
Nick King <nrking0@gmail.com>
2020-10-01 00:52:39 -0500

    Fixed Home Button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css';
 import Button from '@material-ui/core/Button';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {Link} from 'react-router-dom';
 
 
 const Home = (props) => {
@@ -16,7 +17,7 @@ const Home = (props) => {
           <div className="about">
             <div className="aboutH">About</div>
             <div className="aboutP">This is my React Project for the HackIllinois Systems Front-End Challenge. I have utilized the HackIllinois API to have a schedule prepared with all the listed events. You can reach the schedule using the NavBar, or just with the link below. Enjoy!</div>
-            <Button className="button" variant="contained" color="primary" href="/HackIllinoisChallenge/schedule">View Schedule</Button>
+            <Button className="button" variant="contained" color="primary" ><Link style={{color: 'white', textDecoration: 'none'}} to="/HackIllinoisChallenge/schedule">View Schedule</Link></Button>
           </div>
         </div>
       </div>
