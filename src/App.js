1dba526fc3c724151f463e4c738235ad35c80413
Nick King <nrking0@gmail.com>
2020-10-01 00:12:25 -0500

    fixed router

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <div className={classes.root}>
           <Route exact path="/"><Home /></Route>
+          <Route path="/HackIllinoisChallenge"><Home /></Route>
           <Route path="/schedule"><Calendar /></Route>
         </div>
       </Switch>
