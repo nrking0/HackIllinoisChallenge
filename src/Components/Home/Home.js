7b8fcd92948321b856d23fcb277920b3d6498548
Nick King <nrking0@gmail.com>
2020-10-01 00:24:49 -0500

    button link

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,7 +16,7 @@ const Home = (props) => {
           <div className="about">
             <div className="aboutH">About</div>
             <div className="aboutP">This is my React Project for the HackIllinois Systems Front-End Challenge. I have utilized the HackIllinois API to have a schedule prepared with all the listed events. You can reach the schedule using the NavBar, or just with the link below. Enjoy!</div>
-            <Button className="button" variant="contained" color="primary" href="/schedule">View Schedule</Button>
+            <Button className="button" variant="contained" color="primary" href="/HackIllinoisChallenge/schedule">View Schedule</Button>
           </div>
         </div>
       </div>
